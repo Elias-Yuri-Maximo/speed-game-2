03acac23ac5dc9ffa827efc9e5a2dad5f692ace9
NicolasPariente <par21022@byui.edu>
2021-10-29 21:59:02 -0300

    nicolas_update_word_class

--- speed/game/word.py
+++ speed/game/word.py
@@ -1,6 +1,8 @@
 from game import constants
 from game.actor import Actor
 from game.point import Point
+import random
+
 
 class Word(Actor):
     """The responsibility of Word is keep track of this word segments. It contains starting the movement.
@@ -11,36 +13,31 @@ class Word(Actor):
     Attributes:
         _text (List): The word as a string
     """
-    def __init__(self):
+
+    def __init__(self, library):
         """The class constructor.
-        
+
         Args:
             self (Word): An instance of word.
         """
         super().__init__()
-        
-        self._word_list = ['','','','','','','','','','','','','']
-        #should save the word list here 
-        #THIS IS JUST A PLACEHOLDER, NUMBER SHOULD BE GENERATED RANDOMLY
-        self._position = Point(1 , 20)
-       
-        #self._velocity = self. set_velocity(Point(0, 1))
-        self._text = self.set_text(self._prepare_word())
-        print(self._text)
 
-    
-        
-    
+        self._word_list = library
+        # should save the word list here
+        # THIS IS JUST A PLACEHOLDER, NUMBER SHOULD BE GENERATED RANDOMLY
+        self._position = Point(x=random.randint(
+            1, (constants.MAX_X - 2)), y=random.randint(1, (constants.MAX_Y - 2)))
+
+        self._velocity = Point(0, 1)
+        self._text = self.set_text(self._prepare_word())
 
     def _prepare_word(self):
         """Gets the word from the word list and equals it to self.text
-        
+
         Args:
             self (Snake): an instance of Snake.
         """
-        #THIS IS JUST A PLACEHOLDER, THIS SHOULD GET A WORD FROM _WORD_LIST
-        word = 'running word'
+        # THIS IS JUST A PLACEHOLDER, THIS SHOULD GET A WORD FROM _WORD_LIST
+
+        word = random.choice(self._word_list)
         return word
-        
-
-
